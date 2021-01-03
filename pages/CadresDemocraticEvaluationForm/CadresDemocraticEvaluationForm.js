4c9fd733856b63fe371f5fe0ac3477407fc576ca
xxj <1013002924@qq.com>
2021-01-03 22:59:19 +0800

    update

--- pages/CadresDemocraticEvaluationForm/CadresDemocraticEvaluationForm.js
+++ pages/CadresDemocraticEvaluationForm/CadresDemocraticEvaluationForm.js
@@ -1,4 +1,5 @@
 // pages/CadresDemocraticEvaluationForm/CadresDemocraticEvaluationForm.js
+const app = getApp();
 Page({
 
   /**
@@ -6,6 +7,19 @@ Page({
    */
   data: {
     focus: false,
+    questionId:null,
+    accountId:null,
+    optionIds:null,
+    themeName:null,
+    questionArray:null,
+    
+  
+    optionId: null,
+ 
+    remark: '',
+    focus: false,
+    flag: false,
+    
     radioList: [{
         id: 0,
         value: "agree",
@@ -47,22 +61,194 @@ Page({
    * 单项选择
    */
   radiochange: function (e) {
-    console.log('radio发生change事件，携带的value值为：', e.detail.value)
+    // console.log('radio发生change事件，携带的value值为：', e.detail.questionId);
+    // console.log('radio发生change事件，携带的value值为：', e.detail.optionId);
+    var item = e.detail.value //  
+    console.log('radio发生change事件，携带的value值为：', e.detail.value);
+
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    // for (var i = 0; i < item.length; i++) {
+    var row = item.split(",")
+    //将数组进行分割 
+    var questionId = row[0] //数组下表的第一个为
+    var optionId = row[1].concat(',')
+    this.setData({
+      questionId: questionId,
+      optionId: optionId
+    })
+    //数组下表的第二个为name 
+    // }
+    console.log(questionId);
+    console.log(optionId)
+    wx.request({
+      url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        accountId: this.data.accountId,
+        optionIds: this.data.optionId,
+        questionId: this.data.questionId,
+        remark: this.data.remark
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        var e = JSON.parse(res.data.json);
+      }
+    })
+  },
+  /**
+   * 
+   * 多项选择
+   */
+  check(e) {
+    let questionId = e.currentTarget.dataset.id;
+    console.log("当前checkBox事件：", e.currentTarget.dataset.id);
+    this.setData({
+      questionId: questionId
+    })
+    if(this.data.flag==true){
+      this.setData({
+        questionId: questionId,
+        optionId: '',
+        remark:''
+      })
+      wx.request({
+        url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          accountId: this.data.accountId,
+          optionIds:  this.data.optionId,
+          questionId: this.data.questionId,
+          remark: this.data.remark
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+          var e = JSON.parse(res.data.json);
+        }
+
+      })
+    }
+    // if(this.data.newList.contains(""+id)){
+    //     console.log("选中："+id);
+    // } else {
+    //     console.log("取消选中："+id);
+    // }
+  },
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail)
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    var item = e.detail.value //
+
+    var questionId = [];
+
+    var optionId = []; //  选中的NAME
+
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    for (var i = 0; i < item.length; i++) {
+      var row = item[i].split(",")
+      //将数组进行分割 
+      questionId = questionId.concat(row[0]) //数组下表的第一个为
+      optionId = optionId.concat(row[1])
+      //数组下表的第二个为name 
+    }
+    console.log("多选事件", questionId);
+    console.log("多选事件", optionId);
+    if (optionId.length > 0) {
+      var questionId = row[0] //数组下表的第一个为
+      var optionId = row[1].concat(',')
+      this.setData({
+        questionId: questionId,
+        optionId: optionId,
+        flag :false
+      })
+      wx.request({
+        url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          accountId: this.data.accountId,
+          optionIds: this.data.optionId,
+          questionId: this.data.questionId,
+          remark: this.data.remark
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+          var e = JSON.parse(res.data.json);
+        }
+
+      })
+    } else {
+      this.setData({
+        flag :true
+      })
+      console.log("数组为空，多选数组为空");
+    }
+   
+
+
   },
   /**
    * 
    * textarea
    */
   bindTextAreaBlur: function (e) {
-    console.log(e.detail.value);
+    console.log(e.detail.value)
+    // wx.request({
+    //   url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+    //   method: 'post',
+    //   data: {
+    //     accountId: this.data.accountId,
+    //     optionIds: this.data.optionIds,
+    //     questionId:this.data.questionId,
+    //     remark:this.data.remark
+    //   },
+    //   header: {
+    //     'content-type': 'application/x-www-form-urlencoded'
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data);
+    //     var e = JSON.parse(res.data.json);
+    //   }
+    // })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
+    var that = this;
+    that.setData({
+      accountId: wx.getStorageSync('accountId')
+    })
+    wx.request({
+      url: app.globalData.URL + '/app/member-theme2-list.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        accountId: that.data.accountId,
+        // optionIds:  this.data.optionId,
+        // questionId: this.data.questionId,
+        pageno:1
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        var e = JSON.parse(res.data.json);
+        console.log(e);
+        that.setData({
+          themeName: e.themeName,
+          questionArray:e.questionArray
+        })
+      }})
+    },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/DemocraticEvaluationForm/DemocraticEvaluationForm.js
+++ pages/DemocraticEvaluationForm/DemocraticEvaluationForm.js
@@ -8,9 +8,12 @@ Page({
   data: {
     accountId: null, //存放账户id
     themeName: null, //标题名字
-    questionArray:null,
-
+    questionArray: null,
+    optionId: null,
+    questionId: null,
+    remark: '',
     focus: false,
+    flag: false,
     radioList: [{
         id: 0,
         value: "good",
@@ -58,7 +61,7 @@ Page({
         name: '英国'
       }
     ],
-    questionArray_0_optionArray:[
+    questionArray_0_optionArray: [
 
     ],
   },
@@ -67,38 +70,172 @@ Page({
    * 单项选择
    */
   radiochange: function (e) {
-    console.log('radio发生change事件，携带的value值为：', e.detail.value)
+    // console.log('radio发生change事件，携带的value值为：', e.detail.questionId);
+    // console.log('radio发生change事件，携带的value值为：', e.detail.optionId);
+    var item = e.detail.value //  
+    console.log('radio发生change事件，携带的value值为：', e.detail.value);
+
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    // for (var i = 0; i < item.length; i++) {
+    var row = item.split(",")
+    //将数组进行分割 
+    var questionId = row[0] //数组下表的第一个为
+    var optionId = row[1].concat(',')
+    this.setData({
+      questionId: questionId,
+      optionId: optionId
+    })
+    //数组下表的第二个为name 
+    // }
+    console.log(questionId);
+    console.log(optionId)
+    wx.request({
+      url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        accountId: this.data.accountId,
+        optionIds: this.data.optionId,
+        questionId: this.data.questionId,
+        remark: this.data.remark
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        var e = JSON.parse(res.data.json);
+      }
+    })
+  },
+    /**
+   * 
+   * 多项选择
+   */
+  handleJumpPage(e){
+    let questionId = e.currentTarget.dataset.id;
+    console.log("当前handleJumpPage事件：", e.currentTarget.dataset.id);
   },
   /**
    * 
    * 多项选择
    */
+  check(e) {
+    let questionId = e.currentTarget.dataset.id;
+    console.log("当前checkBox事件：", e.currentTarget.dataset.id);
+    this.setData({
+      questionId: questionId
+    })
+    if(this.data.flag==true){
+      this.setData({
+        questionId: questionId,
+        optionId: '',
+        remark:''
+      })
+      wx.request({
+        url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          accountId: this.data.accountId,
+          optionIds:  this.data.optionId,
+          questionId: this.data.questionId,
+          remark: this.data.remark
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+          var e = JSON.parse(res.data.json);
+        }
+
+      })
+    }
+    // if(this.data.newList.contains(""+id)){
+    //     console.log("选中："+id);
+    // } else {
+    //     console.log("取消选中："+id);
+    // }
+  },
   checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail)
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
 
-    const items = this.data.items
-    const values = e.detail.value
-    for (let i = 0, lenI = items.length; i < lenI; ++i) {
-      items[i].checked = false;
-      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (items[i].value === values[j]) {
-          console.log("i,j", i, j);
-          items[i].checked = true;
-          break
-        }
-      }
-    }
+    var item = e.detail.value //
 
-    this.setData({
-      items
-    })
+    var questionId = [];
+
+    var optionId = []; //  选中的NAME
+
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    for (var i = 0; i < item.length; i++) {
+      var row = item[i].split(",")
+      //将数组进行分割 
+      questionId = questionId.concat(row[0]) //数组下表的第一个为
+      optionId = optionId.concat(row[1])
+      //数组下表的第二个为name 
+    }
+    console.log("多选事件", questionId);
+    console.log("多选事件", optionId);
+    if (optionId.length > 0) {
+      var questionId = row[0] //数组下表的第一个为
+      var optionId = row[1].concat(',')
+      this.setData({
+        questionId: questionId,
+        optionId: optionId,
+        flag :false
+      })
+      wx.request({
+        url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          accountId: this.data.accountId,
+          optionIds: this.data.optionId,
+          questionId: this.data.questionId,
+          remark: this.data.remark
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+          var e = JSON.parse(res.data.json);
+        }
+
+      })
+    } else {
+      this.setData({
+        flag :true
+      })
+      console.log("数组为空，多选数组为空");
+    }
+   
+
+
   },
   /**
    * 
    * textarea
    */
   bindTextAreaBlur: function (e) {
+    console.log("okok");
     console.log(e.detail.value)
+    wx.request({
+      url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        accountId: this.data.accountId,
+        optionIds: this.data.optionIds,
+        questionId:this.data.questionId,
+        remark:this.data.remark
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        var e = JSON.parse(res.data.json);
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -125,10 +262,10 @@ Page({
         console.log(e.questionArray);
         that.setData({
           themeName: e.themeName,
-          questionArray:e.questionArray,
-     
+          questionArray: e.questionArray,
+
         })
-        var values=function(object) {
+        var values = function (object) {
           var values = [];
           for (var property in object)
             values.push(object[property]);
